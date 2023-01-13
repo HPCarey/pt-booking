d6913251c118ff8630f37ef6a33a62d51cc83d71
HPCarey <hannahpcarey@gmail.com>
2023-01-13 21:30:00 +0000

    install summernoe

--- booking/admin.py
+++ booking/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Book_Appointment
 from .models import Gender
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Gender)
-admin.site.register(Book_Appointment)
+
+@admin.register(Book_Appointment)
+class AppointmentAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
