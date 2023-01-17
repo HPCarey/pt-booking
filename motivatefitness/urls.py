04a5b1bcdee721f18f7fa703457c7917eb750b2e
HPCarey <hannahpcarey@gmail.com>
2023-01-17 17:28:52 +0000

    implement allauth authorisation and start on navbar

--- motivatefitness/urls.py
+++ motivatefitness/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('', include('booking.urls'), name="booking_urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
