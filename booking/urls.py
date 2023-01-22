8a9a17a961685949286d82c68822431caf869b65
HPCarey <hannahpcarey@gmail.com>
2023-01-22 18:51:23 +0000

    feat: add basic delete functionality

--- booking/urls.py
+++ booking/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('add_booking/', views.add_booking, name='add_booking'),
     path('user_profile/', views.user_profile, name='user_profile'),
     path('update_booking/<id>', views.update_booking, name='update_booking'),
+    path('delete_booking/<id>', views.delete_booking, name='delete_booking'),
+
+
 ]
